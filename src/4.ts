efe3a5c3ef0c0ac9a4e6e54edc1e6a2edea78fb2
Anton Styazhkin <rudana089@gmail.com>
2023-11-13 18:30:54 +0200

    Complete tasks

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,61 @@
-const key = new Key();
+// Клас Key
+class Key {
+    constructor(private signature: number = Math.random()) { }
 
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+// Клас Person
+class Person {
+    constructor(private key: Key) {
+    }
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+// Абстрактний клас House
+abstract class House {
+    protected door: boolean = false;
+    protected tenants: Person[] = [];
+    constructor(protected key: Key) {
+        this.key = key;
+    }
+
+    abstract openDoor(key: Key): void;
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log(`${person.getKey().getSignature()} entered the house.`);
+        } else {
+            console.log('The door is closed. Cannot enter.');
+        }
+    }
+}
+
+// Клас MyHouse, який успадковується від House
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (key.getSignature() > 0.5) {
+            this.door = true;
+            console.log('The door is open.');
+        } else {
+            this.door = false;
+            console.log('Wrong key. The door remains closed.');
+        }
+    }
+}
+
+// Сценарій
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
+house.comeIn(person)
 
-house.comeIn(person);
-
-
-export {};
+export { };
